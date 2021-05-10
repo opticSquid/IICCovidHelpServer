3c26d36b387154957fb1f7771ad6853a18bf040c
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2021-05-10 16:24:54 +0530

    testing location

--- Routes/getHealthCentres.js
+++ Routes/getHealthCentres.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const findHealthCentre = require("../Database/HealthCentre");
 const getHealthCentres = (req, res) => {
   let body = req.body;
+  console.log("Data that came from front end: \n", body);
   //let doc = {Location: <Location/>, Radius: <Radius/> SortBy: <Sortby/>}
   findHealthCentre
     .GetNearbyCenter(body.Location, parseInt(body.Radius), body.SortBy)
     .then((response) => {
+
       console.log("Found this Centres", response);
       res.status(200).json({ Centres: response });
     })
